12c25315f12cec1dedec98c4546da527445313e4
Akanji3 <125752628+Akanji3@users.noreply.github.com>
2023-04-13 11:32:59 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.207.68.233:9000/',
+  serverUrl: 'http://54.152.163.206:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
